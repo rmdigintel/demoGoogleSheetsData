661cd24ee345118ac5431a50c1bd29833a74dff3
mragozina <rm@digintel.ru>
2022-01-11 16:53:08 +0300

    LT-451 Correct MainScenario.kt

--- src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
@@ -2,26 +2,33 @@ package com.justai.jaicf.template.scenario
 
 import com.justai.jaicf.activator.caila.*
 import com.justai.jaicf.builder.Scenario
+import com.justai.jaicf.channel.jaicp.reactions.jaicp
+import com.justai.jaicf.hook.*
 import com.justai.jaicf.template.client
-import com.justai.jaicf.template.scripts.API_KEY
-//import com.justai.jaicf.template.scripts.TAGS_AND_THEIR_CITIES
-import com.justai.jaicf.template.scripts.cailaConform
-import com.justai.jaicf.template.scripts.findFirstDocument
+import com.justai.jaicf.template.scripts.*
 import com.justai.jaicf.test.context.runInTest
-import com.mongodb.client.model.Filters
-import java.awt.Point
-import java.util.ArrayList
+import java.util.*
 
 typealias CitiesAndAmount = Pair<String, Int>
 
 val mainScenario = Scenario {
+    handle<BotRequestHook> {
+        if (!context.session.containsKey("helloMessage")) {
+            reactions.say("Здравствуйте! Я помогу найти направление по вашему запросу.")
+            context.session["helloMessage"] = true
+        }
+    }
+
+    handle<ActionErrorHook> {
+        reactions.say("Извините, возникла ошибка. Попробуйте еще раз")
+    }
 
     state("start") {
         activators {
             regex("/start")
             intent("Hello")
         }
-        activators ("/suggestCity") {
+        activators("/help") {
             intent("Yes")
         }
         action {
@@ -41,36 +48,44 @@ val mainScenario = Scenario {
             }
             // find tag in mongo DB
             val tagWithDestination = findFirstDocument(tag, client.getDatabase("jaicf").getCollection("googleSheets"))
-            val destination = if (tagWithDestination != null) tagWithDestination.getValue("destination") as ArrayList<*> else listOf()
+            val destination = if (tagWithDestination != null) tagWithDestination.getValue("destination") as ArrayList<String> else listOf()
             if (destination.isNotEmpty()) {
-                val (cities, amount) = CitiesAndAmount(destination.joinToString(), destination.size)
+                val (cities, amount) = CitiesAndAmount(destination.sorted().joinToString(), destination.size)
                 val input = request.input
                 val cityConformed = cailaConform("город", amount, API_KEY)
                 reactions.say("По вашему запросу \"$input\" мне удалось найти $amount $cityConformed: $cities")
+                reactions.say("Вы довольны результатом поиска?")
             } else {
-                reactions.say("Я пока не знаю подходящего места по вашему запросу. Может, вам интересно другое направление?")
+                reactions.go("/fallback")
+            }
+        }
+
+        state("yes") {
+            activators {
+                intent("Thanks")
+                intent("Good")
+                intent("Yes")
+            }
+            action {
+                reactions.say("Рад, что вам понравилось!")
+                reactions.go("/help")
+            }
+        }
+
+        state("no") {
+            activators {
+                intent("No")
+            }
+            action {
+                reactions.say("Очень жаль:(")
+                reactions.go("/help")
             }
         }
     }
 
-    state("thanks") {
-        activators {
-            intent("Thanks")
-        }
-        activators ("/suggestCity") {
-            intent("Good")
-        }
+    state("help") {
         action {
-            reactions.say("Рад, что вам понравилось!")
-        }
-    }
-
-    state("no") {
-        activators {
-            intent("No")
-        }
-        action {
-            reactions.say("Очень жаль, что я не смог вам помочь!")
+            reactions.say("Хотите найду для вас что-нибудь еще?")
         }
     }
 
@@ -78,13 +93,24 @@ val mainScenario = Scenario {
         activators {
             intent("Bye")
         }
+        activators("/help") {
+            intent("No")
+        }
         action {
             reactions.say("Обращайтесь снова! До встречи!")
+            reactions.jaicp?.endSession()
+            context.session.clear()
+            context.client.clear()
         }
     }
 
-    fallback {
-        reactions.say("Я пока не знаю подходящего места по вашему запросу. Может, вам интересно другое направление?")
+    state("fallback") {
+        activators {
+            catchAll()
+        }
+        action {
+            reactions.say("Я пока не знаю подходящего места по вашему запросу. Попробуйте ввести другое направление.")
+        }
     }
 }
 
